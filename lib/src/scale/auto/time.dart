3bcf31a455c5b3efa78761dbd23f8c76564c125b
entronad <entronad@gmail.com>
2019-04-28 17:42:14 +0800

    scale

--- lib/src/scale/auto/time.dart
+++
@@ -1,7 +0,0 @@
-import './util.dart' as auto_util;
-
-const maxCount = 6;
-const snapArray = [ 1, 2, 4, 6, 8, 12 ];
-const minuteMs = 60 * 1000;
-const hourMs = 3600 * 1000;
-const dayMs = 24 * 3600 * 1000;
--- lib/src/scale/scale.dart
+++ lib/src/scale/scale.dart
@@ -3,6 +3,14 @@ import 'package:aesth/src/util/measures.dart';
 
 typedef ScaleFormatter<F> = String Function(F value);
 
+class TickObj<F> {
+  TickObj(this.text, this.tickValue, this.value);
+
+  final String text;
+  final F tickValue;
+  final double value;
+}
+
 abstract class Scale<F> extends FieldAttachable {
   Scale({
     String field,
@@ -18,12 +26,50 @@ abstract class Scale<F> extends FieldAttachable {
     fieldList: fieldList,
   );
 
+  final type = 'base';
+
   final ScaleFormatter formatter;
 
   final Range range;
 
   final String alias;
 
-  final List<String> ticks;
+  final List<F> ticks;
   final int tickCount;
+
+  List values = [];
+
+  // Only value param used.
+  String getText(F value) {
+    final formatter = this.formatter;
+    var rst = (formatter != null) ? formatter(value) : value?.toString;
+    rst = rst ?? '';
+    return rst;
+  }
+
+  double scale(F value);
+
+  List<TickObj> getTicks() {
+    final ticks = this.ticks;
+    final rst = <TickObj>[];
+    ticks.forEach((tick) {
+      final obj = TickObj<F>(
+        this.getText(tick),
+        tick,
+        this.scale(tick),
+      );
+      rst.add(obj);
+    });
+    return rst;
+  }
+
+  double rangeMin() => this.range.min;
+
+  double rangeMax() => this.range.max;
+
+  double invert(double value);
+
+  double translate(F value);
+
+  Scale<F> clone();
 }
